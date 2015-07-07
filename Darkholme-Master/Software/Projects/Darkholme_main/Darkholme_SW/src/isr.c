b6f7495644f91616bb2c3dc2ef2f572ecf2c959d
azizbahri <azizbahri90@gmail.com>
2015-07-07 16:31:03 +0100

    corrected include headers
    corrected dependecies for the following
    stm32f10x_map.h
    stm32f10x_type.h
    stm32f10x_lib.h

--- Darkholme-Master/Software/Projects/Darkholme_main/Darkholme_SW/src/isr.c
+++ Darkholme-Master/Software/Projects/Darkholme_main/Darkholme_SW/src/isr.c
@@ -5,10 +5,16 @@
 * Date               : 08/23/2010
 * Description        : functions about Interrupt service routine
 *******************************************************************************/
+/************************* PROJECT DARKHOLME **************************
+* File Name          : main.c
+* Author             : Aziz
+* Version            : V0.0.1
+* Date               : 07/07/2015
+* Description        : Main program body ported for STM32F429I
+*******************************************************************************/
 
 /* Includes ------------------------------------------------------------------*/
-#include "stm32f10x_lib.h"
-#include "stm32f10x_type.h"
+
 #include "common_type.h"
 #include "usart.h"
 #include "isr.h"
