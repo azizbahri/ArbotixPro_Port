b343626cfb98c0744d647b91cd4b9129b620cd24
azizbahri <azizbahri90@gmail.com>
2015-07-07 17:18:25 +0100

    header corrections and assembly macro
    added stm32f4xx.h to give definition to primitive types
    also removed
    #define __disable_interrupt() __disable_irq()
    #define __enable_interrupt()  __enable_irq()
    
    becaus these are already defined in intrinsics.h in IAR embedded system

--- Darkholme-Master/Software/Projects/Darkholme_main/Darkholme_SW/inc/common_type.h
+++ Darkholme-Master/Software/Projects/Darkholme_main/Darkholme_SW/inc/common_type.h
@@ -41,8 +41,8 @@ typedef enum {OFF = 0, ON = !OFF} PowerState;
 
 
 /* Exported macro ------------------------------------------------------------*/
-#define __disable_interrupt() __disable_irq()
-#define __enable_interrupt()  __enable_irq()
+//#define __disable_interrupt() __disable_irq()         //defined in intrinsics.h
+//#define __enable_interrupt()  __enable_irq()          //defined in intrinsics.h
 
 #define WORD_CAST(AA)		(*(u16 *)(&(AA)))
 
